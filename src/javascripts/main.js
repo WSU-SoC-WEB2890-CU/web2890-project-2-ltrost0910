30d2d67fe5d3fceee113c332a71f0fb924390234
Linda Trost <ltrost0910@aol.com>
2024-10-02 13:00:14 -0600

    working on pulling out header and footer and adjusting space on top for body

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,22 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+
+//add nav menu to page
+fetch("navmenu.html")
+  .then((res) => res.text())
+  .then((text) => {
+    let oldelem = document.querySelector("script#js_navmenu")
+    let newelem = document.createElement("div")
+    newelem.innerHTML = text
+    oldelem.parentNode.replaceChild(newelem, oldelem)
+  })
+
+//add footer to page
+fetch("footer.html")
+  .then((res) => res.text())
+  .then((text) => {
+    let oldelem = document.querySelector("script#js_footer")
+    let newelem = document.createElement("div")
+    newelem.innerHTML = text
+    oldelem.parentNode.replaceChild(newelem, oldelem)
+  })
